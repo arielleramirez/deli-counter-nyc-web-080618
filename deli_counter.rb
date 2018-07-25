bab345b2f6a37dcb1534ef8b497a50a8f1e1b5f1
Arielle N Ramirez <ariellenramirez@gmail.com>
2018-07-25 18:37:03 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ def line(katz_deli)
   if katz_deli.empty?
  puts "The line is currently empty."
 else 
-  other_deli = " The line is currently:"
+  other_deli = "The line is currently:"
   katz_deli.each.with_index(1) do |person, i|
       other_deli << " #{i}. #{person}"
 end
